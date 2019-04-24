f4e45da01d3ad663285f2f89cd64461ad4c93810
epam-bivanov <36917628+epam-bivanov@users.noreply.github.com>
2019-04-24 10:52:49 +0300

    Update factorial.java

--- factorial.java
+++ factorial.java
@@ -3,13 +3,12 @@ public class Factoriel
 	public static void main(String[] args)
 	{	final int NUM_FACTS = 100;
 		for(int i = 0; i < NUM_FACTS; i++)
-			System.out.println( i + "! is " + factorial(i));
-	}
-	
-	public static int factorial(int n)
-	{	int result = 1;
-		for(int i = 2; i <= n; i++)
-			result *= i;
-		return result;
+		{
+			int fact = 1;
+			for(int i = 2; i <= n; i++)
+				fact *= i;
+
+			System.out.println( i + "! is " + fact);
+		}
 	}
 }
